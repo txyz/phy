71dac8972f4f16d73be7831daad8c2335b9422f4
yhw1630 <yhw163@yahoo.com>
2016-10-06 15:47:42 -0700

    Update PhysicalUnit.h

--- PhysicalUnit.h
+++ PhysicalUnit.h
@@ -20,7 +20,7 @@
 #ifndef  PHY_PhysicalUnit_INC
 #define  PHY_PhysicalUnit_INC
 
-#include <string>
+#include "Streamable.hpp"
 #include <map>
 
 namespace phy {
@@ -30,24 +30,30 @@ namespace phy {
  *  Description:  
  * =====================================================================================
  */
-class PhysicalUnit
+class PhysicalUnit : public Streamable
 {
   public:
+    static const std::string SYSTEM_MKS;        // metre-kilogram-second system
+    static const std::string SYSTEM_CGS;        // centimetre-gram-second system
+    static const std::string SYSTEM_MTS;        // metre-tonne-second system
+
     /* ====================  LIFECYCLE     ======================================= */
     /* constructor      */
-    PhysicalUnit (const std::string &name, const std::string &system="SI");
+    PhysicalUnit (const std::string &name, const std::string &system=SYSTEM_MKS);
     PhysicalUnit (const PhysicalUnit &other);   /* copy constructor */
     ~PhysicalUnit ();                           /* destructor       */
 
     /* ====================  ACCESSORS     ======================================= */
     std::string GetName() const;
-    std::string GetBaseName() const;
-    double GetScale() const;
+    double GetScale () const { return m_scale; }
+
     /* ====================  MUTATORS      ======================================= */
 
     /* ====================  OPERATORS     ======================================= */
-
     PhysicalUnit& operator = (const PhysicalUnit &other);   /* assignment operator */
+
+    /* ====================  METHODS       ======================================= */
+    std::string ToString() const {return GetName();}
 
   protected:
     /* ====================  METHODS       ======================================= */
@@ -56,11 +62,13 @@ class PhysicalUnit
 
   private:
     /* ====================  METHODS       ======================================= */
-    void InitBaseUnitDict();
+    void InitPhysicalUnitDict();
+    void ParseUnit(const std::string &name, const std::string &system) const;
 
     /* ====================  DATA MEMBERS  ======================================= */
     std::string                                                     m_system;
-    std::map<std::string, int>                                      m_nameDict;
+    double                                                          m_scale;
+    std::map<std::string, int>                                      m_unitDict;
 
     // Store base units [system of measure][name][scale, reference_unit_name]
     static std::map<std::string, std::map<std::string, std::pair<double, std::string> > >   m_baseUnitDict;
